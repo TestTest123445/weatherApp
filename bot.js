d4d639cb5cdaa596d5519f40bd750878b1aa7ef1
TestTest123445 <166025121+TestTest123445@users.noreply.github.com>
2024-04-04 16:04:51 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require('telegraf');
+
+const TOKEN = "7080791089:AAEbHa8gHXwysVfnV0SrHAToltKA6mbZQ6U"
+
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://dreamy-haupia-0f980c.netlify.app/";
+
+bot.start((ctx) => 
+    ctx.reply('Welcome 1234', {
+        reply_markup: {
+            keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+   },
+ })
+);
+
+bot.launch()
+
