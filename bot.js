5e46c4bd1953ed1d253b88cd85901fc75048e68f
TestTest123445 <166025121+TestTest123445@users.noreply.github.com>
2024-04-04 17:27:20 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,13 +1,24 @@
+import dotenv from 'dotenv';
+
+import logger from './config/logger.js';
+import app from './index.js';
+
 const { Telegraf } = require('telegraf');
 
 const TOKEN = "7080791089:AAEbHa8gHXwysVfnV0SrHAToltKA6mbZQ6U"
 
 const bot = new Telegraf(TOKEN);
 
-const port = process.env.PORT || 3000
+
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log('server running on port ' + port)
-})
+  logger.info(`Server running on port ${port}`);
+});
+
 
 const web_link = "https://dreamy-haupia-0f980c.netlify.app/";
 
